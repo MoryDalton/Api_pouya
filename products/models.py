5935fa0d977e060360e319d0590f2c72cc083f9c
morydalton <morydalton20yahoo.com>
2023-06-26 19:41:37 +0330

    edit products model

--- products/models.py
+++ products/models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 class Products(models.Model):
     name = models.CharField(max_length=100, null=False)
-    cost = models.CharField(max_length=100, null=False)
+    code=models.IntegerField()
+    price=models.IntegerField()
+    is_active=models.BooleanField()
+    min_count=models.IntegerField()
+    desc=models.TextField()
+    image=models.CharField()
 
     def __str__(self) -> str:
         return self.name
