04ce752f6e7abd1337405087cc47b5948c3dec53
morydalton <morydalton20yahoo.com>
2023-06-26 18:33:34 +0330

    edit main urls

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include("users.urls")),
-    path('api/products/', include('products.urls'))
+    path('users/', include("users.urls")),
+    path('products/', include('products.urls'))
 ]
