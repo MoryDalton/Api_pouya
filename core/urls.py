a2a75d3f286d28083ed7cd60a05b22a0d4601ba2
morydalton <morydalton20yahoo.com>
2023-09-10 15:26:29 +0330

    delete cache files

--- core/urls.py
+++ core/urls.py
@@ -35,11 +35,11 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include("users.urls")),
-    path('api/products/', include('products.urls')),
-    path('api/cart/', include('cart.urls')),
+    path('users/', include("users.urls")),
+    path('products/', include('products.urls')),
+    path('cart/', include('cart.urls')),
 
-    path('api/', schema_view.with_ui('swagger',
+    path('', schema_view.with_ui('swagger',
          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc', schema_view.with_ui(
         'redoc', cache_timeout=0), name='schema-redoc'),
